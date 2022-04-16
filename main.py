3a3d7f8b9f4e3f4d6914f86e4857ee21bc23a4bc
Christopher Lin <baby.titan.lin@gmail.com>
2022-04-15 22:26:43 -0700

    updated to fit my product, still some errors

--- main.py
+++ main.py
@@ -1,78 +1,76 @@
 from flask import Flask
 from flask_restful import Api, Resource, marshal, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
-#
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+#db = SQLAlchemy(app)
 
-class VideoModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    views = db.Column(db.Integer, nullable=False)
-    likes = db.Column(db.Integer, nullable=False)
+# class VideoModel(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     views = db.Column(db.Integer, nullable=False)
+#     likes = db.Column(db.Integer, nullable=False)
 
-    def __repr__(self):
-        return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes})"
+#     def __repr__(self):
+#         return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes})"
+People = [{"name":"Joe", "age":"30", "id": 0},
+          {"name":"Bob", "age":"15", "id": 1},
+          {"name":"Alice", "age":"10", "id": 2}]
 
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of video is required", required=True)
-video_put_args.add_argument("views", type=int, help="Views of video is required", required=True)
-video_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
+person_put_args = reqparse.RequestParser()
+person_put_args.add_argument("name", type=str, help="Name of person is required", required=True)
+person_put_args.add_argument("age", type=int, help="Age of person is required", required=True)
 
 video_update_args = reqparse.RequestParser()
-video_update_args.add_argument("name", type=str, help="Name of video is required")
-video_update_args.add_argument("views", type=int, help="Views of video is required")
-video_update_args.add_argument("likes", type=int, help="Likes on the video is required")
+video_update_args.add_argument("name", type=str, help="Name of person is required")
+video_update_args.add_argument("age", type=int, help="Age of person is required")
 
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
-    'views': fields.Integer,
-    'likes': fields.Integer
+    'age': fields.Integer
 }
 
-db.create_all() # Eugene
+# db.create_all() # Eugene
 
 class Video(Resource):
-    @marshal_with(resource_fields)
-    def get(self, video_id):
-        result = VideoModel.query.filter_by(id=video_id).first()
+    # @marshal_with(resource_fields)
+    def get(self, person_id):
+        result = People[person_id]
         if not result:
             abort(404, message="Video not found")
         return result
     
-    @marshal_with(resource_fields)
-    def put(self, video_id):
-        args = video_put_args.parse_args()
-        result = VideoModel.query.filter_by(id=video_id).first()
+    # @marshal_with(resource_fields)
+    def put(self, person_id):
+        args = person_put_args.parse_args()
+        result = People[person_id]
         if result:
             abort(409, message="Video ID taken")
-        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-        db.session.add(video)
-        db.session.commit()
-        return video, 201
+        People[person_id] = args
+        
+        return People[person_id], 201
     
-    @marshal_with(resource_fields)
-    def patch(self, video_id):
-        args = video_update_args.parse_args()
-        result = VideoModel.query.filter_by(id=video_id).first()
-        if not result:
-            abort(404, message="Video doesn't exist")
-        if args['name']:
-            result.name = args['name']
-        if args['views']:
-            result.views = args['views']
-        if args['likes']:
-            result.likes = args['likes']
+    # @marshal_with(resource_fields)
+    # def patch(self, video_id):
+    #     args = video_update_args.parse_args()
+    #     result = VideoModel.query.filter_by(id=video_id).first()
+    #     if not result:
+    #         abort(404, message="Video doesn't exist")
+    #     if args['name']:
+    #         result.name = args['name']
+    #     if args['views']:
+    #         result.views = args['views']
+    #     if args['likes']:
+    #         result.likes = args['likes']
         
-        db.session.commit()
+        #db.session.commit()
 
-        return result
+        # return result
 
         
-api.add_resource(Video, "/video/<int:video_id>")
+api.add_resource(Person, "/person/<int:person_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import requests
-#
-#
 BASE = "http://localhost:5000/"
 
 response = requests.patch(BASE + "video/2", {})
